391c5018f6852fdce3a6b3a29fa7d13934bfff9b
archiecheng <archiechengice@outlook.com>
2024-05-08 12:04:10 -0700

    设计模式-单一职责原则

---
+++ src/singleresponsibility/SingleResponsibility1.java
@@ -0,0 +1,30 @@
+package singleresponsibility;
+
+/**
+ * @author Archie Cheng
+ * @version V1.0.0
+ * @projectName Design Pattern
+ * @title SingleResponsibility1
+ * @package singleresponsibility
+ * @description
+ * @date 2024/5/8 11:32
+ * @copyright 2024
+ */
+public class SingleResponsibility1 {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.run("摩托车");
+        vehicle.run("汽车");
+        vehicle.run("飞机");
+    }
+}
+
+// 交通工具类
+// 方式1
+// 1. 在方式1的 run 方法中, 违反了单一职责原则
+// 2. 解决的方式非常简单, 根据交通工具运行的方式不同, 分解为不同的类即可
+class Vehicle {
+    public void run(String vehicle) {
+        System.out.println(vehicle + "在公路上运行...");
+    }
+}
---
+++ src/singleresponsibility/SingleResponsibility2.java
@@ -0,0 +1,44 @@
+package singleresponsibility;
+
+/**
+ * @author Archie Cheng
+ * @version V1.0.0
+ * @projectName Design Pattern
+ * @title SingleResponsibility2
+ * @package singleresponsibility
+ * @description
+ * @date 2024/5/8 11:36
+ * @copyright 2024
+ */
+public class SingleResponsibility2 {
+    public static void main(String[] args) {
+        RoadVehicle roadVehicle = new RoadVehicle();
+        roadVehicle.run("汽车");
+        roadVehicle.run("摩托车");
+        AirVehicle airVehicle = new AirVehicle();
+        airVehicle.run("飞机");
+
+    }
+}
+
+// 方案2的一个分析
+// 1. 遵守了单一职责原则
+// 2. 但是这样做改动很大, 即需要将类分解, 同时修改客户端
+// 3. 改进: 直接修改 Vehicle 类, 改动的代码会比较少
+class RoadVehicle {
+    public void run(String vehicle){
+        System.out.println(vehicle + "在公路上运行....");
+    }
+}
+
+class AirVehicle {
+    public void run(String vehicle){
+        System.out.println(vehicle + "在天上上运行....");
+    }
+}
+
+class WaterVehicle {
+    public void run(String vehicle){
+        System.out.println(vehicle + "在水中上运行....");
+    }
+}
